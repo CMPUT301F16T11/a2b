eece5ac2672fd76ee536721578b0209d974b444b
scarletspider616 <j.fallone@ualberta.ca>
2016-10-22 17:58:37 -0600

    Finished updating unit tests

--- app/src/test/java/com/cmput301f16t11/a2b/Story17UnitTest.java
+++ app/src/test/java/com/cmput301f16t11/a2b/Story17UnitTest.java
@@ -8,30 +8,35 @@ import static org.junit.Assert.assertEquals;
 
 public class Story17UnitTest {
 
-/* Based on US 05.03.01
-As a driver, I want to see if my acceptance was accepted.
-*/
+    /* Based on US 05.03.01
+    As a driver, I want to see if my acceptance was accepted.
+    */
 
-    User driver;
-    String username = "username";
-    String start = "here";
-    String end = "there";
-    int fare = 10;
+    /*
+    see test story 8.
+    gui tests? - "see"
+     */
 
-    @Test
-    public void testConfirmAccepted() {
-        //Login as driver
-        driver = UserController.loadUser(username);
-        //Look into list of pending accepted requests
-        ArrayList<UserRequest> requestList = driver.getRequests();
-        UserRequest request = new UserRequest(start,end,fare);
-        request.setAcceptedStatus(true);
-        requestList.add(request);
-
-        //see that a pending request has been accepted
-        assertEquals(request.getAcceptedStatus(), true);
-        //assertequals on the wanted vs. actual
-
-    }
+//    User driver;
+//    String username = "username";
+//    String start = "here";
+//    String end = "there";
+//    int fare = 10;
+//
+//    @Test
+//    public void testConfirmAccepted() {
+//        //Login as driver
+//        driver = UserController.loadUser(username);
+//        //Look into list of pending accepted requests
+//        ArrayList<UserRequest> requestList = driver.getRequests();
+//        UserRequest request = new UserRequest(start,end,fare);
+//        request.setAcceptedStatus(true);
+//        requestList.add(request);
+//
+//        //see that a pending request has been accepted
+//        assertEquals(request.getAcceptedStatus(), true);
+//        //assertequals on the wanted vs. actual
+//
+//    }
 
 }
