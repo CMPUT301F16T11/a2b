d07ada54aa3363887f91d0b3668d24c91335da66
Tother89 <toth.dallin@gmail.com>
2016-11-03 22:20:40 -0600

    added profile activity

---
+++ app/src/main/java/com/cmput301f16t11/a2b/ProfileActivity.java
@@ -0,0 +1,26 @@
+package com.cmput301f16t11.a2b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+    public void upDateActivity(){
+
+    }
+
+    private void logout(){
+
+    }
+    private void launchProfileEditor(){
+
+    }
+}
