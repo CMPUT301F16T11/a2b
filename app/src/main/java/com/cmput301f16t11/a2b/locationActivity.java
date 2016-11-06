7c6f3931cda8e889ea637d7cc91d1776f7b18d53
symborsk <symborsk@ualberta.ca>
2016-11-05 19:57:37 -0600

    Added a way for a user to set a start and finish point in the main map activity for their trip. They can set start and end location and cancel their selection at any point.

--- app/src/main/java/com/cmput301f16t11/a2b/locationActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/locationActivity.java
@@ -1,6 +1,7 @@
 package com.cmput301f16t11.a2b;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -10,6 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,8 +20,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -28,38 +32,130 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class locationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker tripStartMarker;
+    private Marker tripEndMarker;
+    private Marker currentMarker;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
-        setButtonListeners();
+        context = this;
     }
 
     /**
      * Set up all the on click listeners for this activity
      */
     private void setButtonListeners(){
-        Button editProfileButton = (Button)findViewById(R.id.editProfile);
-        Button setLocation = (Button)findViewById(R.id.setLocationButton);
+        final Button editProfileButton = (Button)findViewById(R.id.editProfile);
+        final Button setLocation = (Button)findViewById(R.id.setLocationButton);
+        final Button cancelTrip = (Button)findViewById(R.id.cancelTrip);
+        cancelTrip.setEnabled(false);
 
-        editProfileButton.setOnClickListener(new View.OnClickListener() {
+        cancelTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: edit profile code goes here
+                if(tripStartMarker != null){
+                    tripStartMarker.remove();
+                    tripStartMarker = null;
+                }
+                if(tripEndMarker != null){
+                    tripEndMarker.remove();
+                    tripEndMarker = null;
+                }
+                if(currentMarker != null){
+                    currentMarker.remove();
+                    currentMarker = null;
+                }
+
+                //Reset button state when they cancel
+                setLocation.setText(R.string.set_start);
+
+                //Once they cancel it disable this button until they place another pin
+                cancelTrip.setEnabled(false);
             }
         });
 
         setLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: setting start and end location of trip goes here
+                //Do not want to spawn a warning if the end marker is not null
+                if(currentMarker == null && tripEndMarker == null){
+                    AlertDialog markerWarning = new AlertDialog.Builder(context).create();
+                    markerWarning.setMessage(getString(R.string.warning_message));
+                    markerWarning.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    markerWarning.show();
+                }
+
+                //We know we are setting our start
+                else if(tripStartMarker == null){
+                    currentMarker.remove();
+                    tripStartMarker = mMap.addMarker(new MarkerOptions()
+                            .position(currentMarker.getPosition())
+                            .title(getString(R.string.start_location))
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+                    tripStartMarker.showInfoWindow();
+                    currentMarker = null;
+                    setLocation.setText(R.string.set_end);
+                    cancelTrip.setEnabled(true);
+                }
+
+                else if(tripEndMarker == null){
+                   currentMarker.remove();
+                    tripEndMarker = mMap.addMarker(new MarkerOptions()
+                            .position(currentMarker.getPosition())
+                            .title(getString(R.string.end_location))
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                    tripEndMarker.showInfoWindow();
+                    currentMarker = null;
+                    setLocation.setText(R.string.confirm_trip);
+
+                }
+
+                else{
+                    //TODO: maybe some drawing between these two points
+                    //TODO: confirmation of trip do something with the lat and long
+                }
+
+            }
+        });
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: edit profile activity goes here
+            }
+        });
+
+        //Listener for the map so we know when the user clicks
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                if (currentMarker != null) {
+                    currentMarker.remove();
+                }
+                if (tripEndMarker == null) {
+                    currentMarker = mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                }
             }
         });
     }
@@ -77,8 +173,10 @@ public class locationActivity extends FragmentActivity implements OnMapReadyCall
                 .target(edmonton)      // Sets the center of the map to location user
                 .zoom(11)                   // Sets the zoom
                 .bearing(90)                // Sets the orientation of the camera to east
-                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .tilt(0)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        setButtonListeners();
     }
 }
