72671c76adfb0375a5bdbf24bec712d669ed22d9
scarletspider616 <j.fallone@ualberta.ca>
2016-11-07 12:12:04 -0700

    First skeleton of requestlist code

--- app/src/main/java/com/cmput301f16t11/a2b/RequestController.java
+++ app/src/main/java/com/cmput301f16t11/a2b/RequestController.java
@@ -1,12 +1,19 @@
 package com.cmput301f16t11.a2b;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 
 /**
  * Created by brianofrim on 2016-10-13.
  */
 public class RequestController {
-    public static ArrayList<UserRequest> getRequestNear(String address, Number radius){
-        return new ArrayList<UserRequest>();
+
+    public static ArrayList<Request> getRequestNear(String address, Number radius){
+        return new ArrayList<Request>();
+    }
+
+    public static ArrayList<Request> getRequestNear(LatLng location, Number radius) {
+        return new ArrayList<Request>();
     }
 }
