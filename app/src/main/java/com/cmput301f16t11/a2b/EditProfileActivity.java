22b98a5730135f63c9658f6adf80559382b7176a
liangKelvin <liangmah@ualberta.ca>
2016-11-07 16:17:31 -0700

    touched up editprofile

--- app/src/main/java/com/cmput301f16t11/a2b/EditProfileActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/EditProfileActivity.java
@@ -10,36 +10,32 @@ public class EditProfileActivity extends AppCompatActivity {
 
 
     User user;
+    //TODO : WE can consider habing profile picture implementation later for part 5
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         // sets the edit text views of the profile view
-        //setTexts();
-        user = UserController.getUser();
+        setTexts();
 
-        // populate with existing info
-
-        // Not sure if name should be editable
-        EditText editText = (EditText) findViewById(R.id.profile_name);
-        editText.setText(user.getName(), TextView.BufferType.EDITABLE);
-        // Not sure if we need address here
-        editText = (EditText) findViewById(R.id.address);
-        editText.setText("insert address of user here", TextView.BufferType.EDITABLE);
-        editText = (EditText) findViewById(R.id.phone_num);
-        editText.setText(user.getPhoneNumber(), TextView.BufferType.EDITABLE);
-        editText = (EditText) findViewById(R.id.email);
-        editText.setText(user.getEmail(), TextView.BufferType.EDITABLE);
 
     }
 
     public void setTexts() {
         // Edit Texts should be set to what the current profile has
 
+        user = UserController.getUser();
+
+        // populate with existing info
+        EditText editText = (EditText) findViewById(R.id.phone_num);
+        editText.setText(user.getPhoneNumber(), TextView.BufferType.EDITABLE);
+        editText = (EditText) findViewById(R.id.email);
+        editText.setText(user.getEmail(), TextView.BufferType.EDITABLE);
+        TextView text = (TextView) findViewById(R.id.profile_name);
+        text.setText(user.getName());
+
     }
-
-
 
     public void editProfile(View v) {
         EditText userPhoneNumText = (EditText) findViewById(R.id.phone_num);
