6bdbbffcf2dab75677dffd2d590553babc9262f7
scarletspider616 <j.fallone@ualberta.ca>
2016-11-07 12:15:30 -0700

    Changed mainactivity back to driverlocationactivity launching for testing

--- app/src/main/java/com/cmput301f16t11/a2b/MainActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TEMPORARY CODE - LAUNCHES the location activity for testing
-        Intent intent = new Intent(this, RequestActivity.class);
+        Intent intent = new Intent(this, driverLocationActivity.class);
         startActivity(intent);
     }
 }
