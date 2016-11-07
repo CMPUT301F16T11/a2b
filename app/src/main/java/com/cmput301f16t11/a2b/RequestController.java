5cd5ec7a4835d5a4847a145de989d1f83ecff221
scarletspider616 <j.fallone@ualberta.ca>
2016-11-07 13:15:57 -0700

    Need to switch computers, uploading changes

--- app/src/main/java/com/cmput301f16t11/a2b/RequestController.java
+++ app/src/main/java/com/cmput301f16t11/a2b/RequestController.java
@@ -28,4 +28,35 @@ public class RequestController {
                 new LatLng(53.5443890, -113.4909270), new User("test2", "test2@email.com")));
         return returnValue;
     }
+
+    public static ArrayList<Request> getNearbyRequests(LatLng location) {
+        //TODO: actual logic
+        ArrayList<Request> temp = new ArrayList<Request>();
+        temp.add(RequestController.tempFakeRequestList().get(0));
+        return temp;
+    }
+
+    public static ArrayList<Request> getOwnRequests(User user) {
+        // TODO: actual logic
+        // (gets requests created by this user)
+        ArrayList<Request> temp = new ArrayList<Request>();
+        temp.add(RequestController.tempFakeRequestList().get(1));
+        return temp;
+    }
+
+    public static ArrayList<Request> getAcceptedByDrivers(User user) {
+        //TODO: actual logic
+        // (get requests accepted by a driver created by this user
+        return new ArrayList<Request>();
+    }
+
+    public static ArrayList<Request> getConfirmedByRiders(User user) {
+        //TODO: actual logic
+        // (get request accepted by the current user as a driver, that are also accepted by the user
+        // who originally made the request
+        ArrayList<Request> temp = new ArrayList<Request>();
+        temp.add(RequestController.tempFakeRequestList().get(2));
+        return temp;
+    }
+
 }
