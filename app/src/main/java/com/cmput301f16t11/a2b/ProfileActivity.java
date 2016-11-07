4f80292d42f86c167149814af306d025c207221c
liangKelvin <liangmah@ualberta.ca>
2016-11-06 17:03:20 -0700

    working on editProfile

--- app/src/main/java/com/cmput301f16t11/a2b/ProfileActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/ProfileActivity.java
@@ -1,7 +1,11 @@
 package com.cmput301f16t11.a2b;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -10,6 +14,15 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // adding edit button functionality
+        Button edit = (Button) findViewById(R.id.editProfile);
+        edit.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                launchProfileEditor();
+            }
+        });
 
     }
 
@@ -20,7 +33,12 @@ public class ProfileActivity extends AppCompatActivity {
     private void logout(){
 
     }
-    private void launchProfileEditor(){
-
+    private void launchProfileEditor() {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        // TODO user info must be put into the intent so that editprofile knows whats being edited
+        //intent.putExtra("user", user);
+        setResult(Activity.RESULT_OK, intent);
+        startActivity(intent);
     }
+
 }
