33ab35a7b2e137325233925f6c0045e9e5c6e2bc
symborsk <symborsk@ualberta.ca>
2016-11-09 16:27:41 -0700

    Updated the edit profile dialog to have an A2B image. Added some input checking on when the user edits email or phone number.

--- app/src/main/java/com/cmput301f16t11/a2b/EditProfileActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/EditProfileActivity.java
@@ -1,10 +1,14 @@
 package com.cmput301f16t11.a2b;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -18,40 +22,106 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
         // sets the edit text views of the profile view
         setTexts();
-
-
     }
 
     public void setTexts() {
         // Edit Texts should be set to what the current profile has
-
         user = UserController.getUser();
 
         // populate with existing info
-        EditText editText = (EditText) findViewById(R.id.phone_num);
-        editText.setText(user.getPhoneNumber(), TextView.BufferType.EDITABLE);
-        editText = (EditText) findViewById(R.id.email);
-        editText.setText(user.getEmail(), TextView.BufferType.EDITABLE);
-        TextView text = (TextView) findViewById(R.id.profile_name);
-        text.setText(user.getName());
+        EditText phoneNum = (EditText) findViewById(R.id.phone_num);
+        EditText email = (EditText) findViewById(R.id.email);
+        TextView username = (TextView) findViewById(R.id.profile_name);
 
+        phoneNum.setText(user.getPhoneNumber(), TextView.BufferType.EDITABLE);
+        email.setText(user.getEmail(), TextView.BufferType.EDITABLE);
+        username.setText(user.getName());
     }
 
     public void editProfile(View v) {
         EditText userPhoneNumText = (EditText) findViewById(R.id.phone_num);
         EditText userEmailText = (EditText) findViewById(R.id.email);
 
+        String phoneNumber = userPhoneNumText.getText().toString();
+        String email = userEmailText.getText().toString();
+
+        AlertDialog dlg = new AlertDialog.Builder(this).create();
+        dlg.setTitle("Cannot Update Profile");
+
+        boolean isValid = !isValidPhoneNumber(phoneNumber);
+
+        if(!isValidEmail(email)){
+            //Warn the user that you cannot update it with invalid info
+            dlg.setMessage("The email is not valid please input a valid email.");
+            dlg.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+            dlg.show();
+            return;
+        }
+
+        else
+        if(!isValidPhoneNumber(phoneNumber)){
+            //Warn the user that you cannot
+            dlg.setMessage("The phone number is not valid please input a valid email.");
+            dlg.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+            dlg.show();
+            return;
+        }
+
+        Boolean changedPhoneNumber = phoneNumber.equals(user.getPhoneNumber());
+        Boolean changedEmail = email.equals(user.getEmail());
+
         //check if any updates have actually been made
-        if(userPhoneNumText.getText().toString().equals(user.getName()) == false  || userEmailText.getText().toString() != user.getPhoneNumber()){
-            user.setPhoneNumber(userPhoneNumText.getText().toString());
-            user.setEmail(userEmailText.getText().toString());
+        if(changedEmail || changedPhoneNumber){
+
+            user.setPhoneNumber(phoneNumber);
+            user.setEmail(email);
+            UserController.setUser(user);
             UserController.updateUserInDb();
         }
 
-        this.finish();
+        finish();
     }
 
     public void cancelEdit(View view) {
+
         finish();
     }
+
+    private Boolean isValidPhoneNumber(String number){
+        Pattern format1 = Pattern.compile("[0-9]{10}"); // 7801234567
+        Pattern format2 = Pattern.compile("[0-9]{3}\\-[0-9]{3}\\-[0-9]{4}");
+
+        if (format1.matcher(number).matches() || format2.matcher(number).matches()) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean isValidEmail(String email){
+        Pattern format = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+        if(format.matcher(email).matches()){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void clearEditText(View v){
+        EditText text = (EditText)v;
+        text.setText("");
+    }
+
 }
