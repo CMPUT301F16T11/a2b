3ccb20fdc62a6838d49dd73d70da9a5b461215b9
BrianOfrim <bofrim@ualberta.ca>
2016-11-09 17:28:36 -0700

    debug code removed from main activity

--- app/src/main/java/com/cmput301f16t11/a2b/MainActivity.java
+++ app/src/main/java/com/cmput301f16t11/a2b/MainActivity.java
@@ -17,56 +17,6 @@ public class MainActivity extends AppCompatActivity {
         //Intent intent = new Intent(this, RiderLocationActivity.class);
         // startActivity(intent)
 
-       User user = new User("someone2","asgasd@asgs.ca","666");
-        user.setId("asvearve435");
-////
-       UserRequest mockReq1 = new UserRequest(new LatLng(113.526230,53.523201),new LatLng(99.999999,88.888888),10,user);
-//        //UserRequest mockReq2 = new UserRequest(new LatLng(113.526130,53.523200),new LatLng(99.999999,88.888888),10,user);
-////
-        UserRequest ur2 =new UserRequest(new LatLng(113.526130,53.523200),new LatLng(99.999999,88.888888),10,user);
-        ElasticsearchRequestController.AddOpenRequestTask addOpenRequestTask = new ElasticsearchRequestController.AddOpenRequestTask();
-       ElasticsearchRequestController.MoveToInProgresseRequest moveToInProgresseRequest = new ElasticsearchRequestController.MoveToInProgresseRequest();
-       ElasticsearchRequestController.MoveToClosedRequest moveToClosedRequest = new ElasticsearchRequestController.MoveToClosedRequest();
-        ElasticsearchRequestController.GetRequest getRequest = new ElasticsearchRequestController.GetRequest();
-////
-       try {
-            addOpenRequestTask.execute(mockReq1).get();
-           //getRequest.execute(mockReq1.getId()).get();
-////
-        }catch(Exception e){
-////
-        }
-
-        try {
-            UserRequest ur3 = getRequest.execute(mockReq1.getId()).get();
-            //getRequest.execute(mockReq1.getId()).get();
-            int i = 1+1;
-            int j = i + 5;
-////
-        }catch(Exception e) {
-
-        }
-////
-
-        try {
-            moveToInProgresseRequest.execute(mockReq1).get();
-            //getRequest.execute(mockReq1.getId()).get();
-////
-        }catch(Exception e){
-////
-        }
-
-
-
-        try {
-            moveToClosedRequest.execute(mockReq1).get();
-            //getRequest.execute(mockReq1.getId()).get();
-////
-        }catch(Exception e){
-////
-        }
-
-
 
 
 
